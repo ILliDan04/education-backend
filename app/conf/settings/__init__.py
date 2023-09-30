927d8441965edc54b24ee6454b34f05063978bc8
Anton Chak <antonchak@Antons-MacBook-Pro.local>
2023-09-30 14:37:13 +0200

    Push pre-commit fixes

--- app/conf/settings/__init__.py
+++ app/conf/settings/__init__.py
@@ -1,5 +1,6 @@
-from conf.settings.django import *
 import os
+
+from conf.settings.django import *  # noqa: F403
 
 GDAL_LIBRARY_PATH = os.environ.get("GDAL_LIBRARY_PATH")
 GEOS_LIBRARY_PATH = os.environ.get("GEOS_LIBRARY_PATH")
